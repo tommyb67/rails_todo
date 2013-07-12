e83cb370ec2965a9f85f28f46c341498e2dc5336
tommyb67 <tommybrennan@gmail.com>
2013-07-12 15:21:49 -0400

    trying to deploy to heroku

--- rails_todo_app/db/migrate/20130708223203_create_todos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTodos < ActiveRecord::Migration
-  def change
-    create_table :todos do |t|
-      t.string :name
-      t.string :description
-      t.string :due_date
-    end
-  end
-end
